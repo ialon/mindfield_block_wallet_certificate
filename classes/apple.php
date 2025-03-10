c8b6ca4dec6a19c76a7646fdbdbfb0b80d057d1b
Josemaria Bolanos <josemabol@gmail.com>
2025-03-10 14:08:47 -0400

    Add generation to Apple/Google classes

--- classes/apple.php
+++ classes/apple.php
@@ -1,5 +1,14 @@
 <?php
 
+namespace block_wallet_certificate;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once(__DIR__ . '/../../../config.php');
+require_once($CFG->dirroot . '/blocks/wallet_certificate/vendor/autoload.php');
+
+use PKPass\PKPass;
+
 /**
  * Wallet Certificate block
  *
@@ -7,8 +16,33 @@
  * @copyright  2025 Josemaria Bolanos <admin@mako.digital>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 */
-class apple {
-    public function get_apple_link() {
-        return 'https://example.com/apple';
+class apple extends base {
+    public public function __construct(int $issueid) {
+        parent::__construct($issueid, 'apple');
+    }
+
+    public function generate_pass() {
+        $pass = new PKPass($this->config->certificatepath, $this->config->certificatepassword);
+
+        // Pass content
+        $data = [
+            'formatVersion' => 1,
+            'passTypeIdentifier' => $this->config->passtypeidentifier,
+            'teamIdentifier' => $this->config->teamidentifier,
+            'barcode' => [
+                'format' => 'PKBarcodeFormatQR',
+                'messageEncoding' => 'iso-8859-1',
+            ]
+        ];
+
+        $pass->addFile('pix/thumbnail.png');
+
+        // Add certificate specific data
+        require_once(__DIR__ . '/certificates/apple/' . $type . '.php');
+
+        $pass->setData($data);
+
+        // Create and output the pass
+        $pass->create(true);
     }
 }
--- classes/google.php
+++ classes/google.php
@@ -1,5 +1,12 @@
 <?php
 
+namespace block_wallet_certificate;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once(__DIR__ . '/../../../config.php');
+require_once($CFG->dirroot . '/blocks/wallet_certificate/vendor/autoload.php');
+
 /**
  * Wallet Certificate block
  *
@@ -7,8 +14,47 @@
  * @copyright  2025 Josemaria Bolanos <admin@mako.digital>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 */
-class google {
-    public function get_google_link() {
-        return 'https://example.com/google';
+class google extends base {
+    public public function __construct(int $issueid) {
+        parent::__construct($issueid, 'google');
+    }
+
+    public function generate_pass() {
+        // Your Issuer account ID
+        $issuerId = $this->config->issuerid;
+
+        // Create a class instance
+        // $generic = new \DemoGeneric($this->config->keyfilepath);
+
+        // // Create a pass class
+        // $generic->createClass($issuerId, 'class_suffix');
+
+        // // Update a pass class
+        // $generic->updateClass($issuerId, 'class_suffix');
+
+        // // Patch a pass class
+        // $generic->patchClass($issuerId, 'class_suffix');
+
+        // // Add a message to a pass class
+        // // $generic->addClassMessage($issuerId, 'class_suffix', 'header', 'body');
+
+        // // Create a pass object
+        // $generic->createObject($issuerId, 'class_suffix', 'object_suffix');
+
+        // // Update a pass object
+        // $generic->updateObject($issuerId, 'object_suffix');
+
+        // // Patch a pass object
+        // $generic->patchObject($issuerId, 'object_suffix');
+
+        // // Add a message to a pass object
+        // // $generic->addObjectMessage($issuerId, 'object_suffix', 'header', 'body');
+
+        // // Expire a pass object
+        // $generic->expireObject($issuerId, 'object_suffix');
+
+        // // Generate an Add to Google Wallet link that creates a new pass class and object
+        // $generic->createJWTNewObjects($issuerId, 'class_suffix', 'object_suffix');
+
     }
 }
